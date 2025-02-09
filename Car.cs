1a30c1968e6e43d23374a462dae318b958ede4ba
priyadharshini <poppriyadharshini25@gmail.com>
2025-02-09 22:29:30 +0800

    Car class created to The Car class has properties such as Name, X, Y, and Facing, where X and Y represent the car's current position, and Facing represents the direction the car is fa

---
+++ Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDrivingCarSimulation
+{
+    public class Car
+    {
+        public string Name
+        {
+            get;
+        }
+        public int X
+        {
+            get;
+            private set;
+        }
+        public int Y
+        {
+            get;
+            private set;
+        }
+        public string Facing
+        {
+            get;
+            private set;
+        }
+
+        private int Width
+        {
+            get;
+        }
+        private int Height
+        {
+            get;
+
+        }
+
+    }
+}
