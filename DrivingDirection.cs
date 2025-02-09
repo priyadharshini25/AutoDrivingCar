0baffe13076be5a7cf07fa4d5f18735dfa0d76e6
priyadharshini <poppriyadharshini25@gmail.com>
2025-02-09 22:13:09 +0800

    DrivingDirection.cs

---
+++ DrivingDirection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoDrivingCarSimulation
+{
+    // This class indicates the 4 direction the car can travel
+    internal class DrivingDirection
+    {
+            public const string N = "North";
+            public const string E = "East";
+            public const string S = "South";
+            public const string W = "West";
+    }
+
+}
